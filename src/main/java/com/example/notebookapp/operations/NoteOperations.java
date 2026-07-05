14e07cbb9a7aa8665ed7098ddd1c86c7574d6c7d
Adam <algolec1990@yahoo.com>
2026-07-05 19:40:57 -0400

    Extract menu printing methods, console input methods, and create operations packages for notes and folders, as well as display methods for folder and notes. Separating existing methods inside NotebookConsole into separate responsibilities.

--- src/main/java/com/example/notebookapp/operations/NoteOperations.java
+++ src/main/java/com/example/notebookapp/operations/NoteOperations.java
@@ -1,2 +1,78 @@
-package com.example.notebookapp.operations;public class NoteOperations {
+package com.example.notebookapp.operations;
+
+import com.example.notebookapp.model.Folder;
+import com.example.notebookapp.model.Note;
+import com.example.notebookapp.ui.input.ConsoleInput;
+import com.example.notebookapp.ui.input.InputValidation;
+import com.example.notebookapp.ui.menu.MenuPrinter;
+
+import java.util.Scanner;
+
+public class NoteOperations {
+    public static void createNote(Folder currentFolder , Scanner kb) {
+        String title = ConsoleInput.enterNoteTitle(kb);
+        String body = ConsoleInput.enterNoteBody(kb);
+
+        Note note = new Note(title, body);
+
+        currentFolder.addNote(note);
+
+        System.out.println("Note created.");
+    }
+
+
+    public static void renameNote(Note note,Scanner kb){
+        String newName;
+        System.out.println("Enter new name for note.");
+        System.out.print("> ");
+        newName = kb.nextLine();
+        String renameSuccess = note.renameTitle(newName) ? "Rename successful" : "Rename unsuccessful";
+        System.out.println(renameSuccess);
+    }
+
+    public static void editNote(Note note,Scanner kb){
+
+        String input;
+
+        do{
+
+            MenuPrinter.printEditNoteMenu();
+            System.out.println("Select an operation or b to back out.");
+            input = kb.nextLine();
+
+
+        } while(InputValidation.isNotDigit(input) && !input.equalsIgnoreCase("b"));
+
+        switch(input){
+            case "1":
+                renameNote(note,kb);
+                break;
+            case "2":
+                removeBody(note,kb);
+                break;
+            case "b":
+                break;
+            case"default":
+                System.out.println("Not a valid option.");
+                break;
+
+        }
+    }
+    public static void removeBody(Note note,Scanner kb){
+        String input;
+        do{
+            System.out.println("Remove body to '" + note.getTitle() + "'?");
+            input = kb.nextLine().trim().toLowerCase();
+
+        }while(input.charAt(0) != 'y' && input.charAt(0) != 'n');
+        if(input.charAt(0) == 'y'){
+            note.setBody("");
+        }
+        if(note.getBody().isEmpty()){
+            String newBody = ConsoleInput.enterNoteBody(kb);
+            note.setBody(newBody);
+            System.out.println("New Body set.");
+        }
+
+    }
 }
--- src/main/java/com/example/notebookapp/ui/display/NoteDisplay.java
+++ src/main/java/com/example/notebookapp/ui/display/NoteDisplay.java
@@ -1,2 +1,39 @@
-package com.example.notebookapp.ui.display;public class NoteDisplay {
+package com.example.notebookapp.ui.display;
+
+import com.example.notebookapp.model.Folder;
+import com.example.notebookapp.model.Note;
+
+public class NoteDisplay {
+    public static void listNotes(Folder currentFolder, boolean verbose){
+
+        int i = 0;
+
+        if(currentFolder.getNotes().isEmpty()){
+            System.out.println("no notes.");
+            return;
+        }
+
+        if(verbose){
+            for(Note note: currentFolder.getNotes()){
+                System.out.println("----");
+                System.out.println(note);
+                System.out.println("----");
+            }
+            return;
+        }
+        System.out.println("\n");
+        for(Note note: currentFolder.getNotes()){
+            System.out.println((i+1)+". " + note.getTitle());
+            i++;
+        }
+
+    }
+    public static void openNote(Note note){
+        if(note == null){
+            System.out.println("Error: note is null.");
+            return;
+        }
+        System.out.println(note.getTitle());
+        System.out.println(note.getBody());
+    }
 }
