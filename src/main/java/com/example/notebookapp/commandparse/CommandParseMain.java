0c3630fcbd7eb56ff31370b25d36aacac4883fdb
Adam Golec <algolec1990@yahoo.com>
2026-07-11 16:23:39 -0400

    add new console input to be used as the foundational basis of using console command input

---
+++ src/main/java/com/example/notebookapp/commandparse/CommandParseMain.java
@@ -0,0 +1,13 @@
+package com.example.notebookapp.commandparse;
+
+import com.example.notebookapp.commandparse.command.ConsoleInput;
+
+public class CommandParseMain {
+    public static void main(String[] args){
+        ConsoleInput in = ConsoleInput.getInstance();
+
+        String input = in.getLine("Enter your string: ");
+        System.out.println("You entered '" + input + "'");
+
+    }
+}
---
+++ src/main/java/com/example/notebookapp/commandparse/command/ConsoleInput.java
@@ -0,0 +1,27 @@
+package com.example.notebookapp.commandparse.command;
+
+import java.io.Console;
+import java.util.Scanner;
+
+public class ConsoleInput {
+    private final Scanner scanner;
+    private static ConsoleInput instance;
+    private ConsoleInput(){
+        this.scanner = new Scanner(System.in);
+    }
+    public static ConsoleInput getInstance(){
+        if(instance == null){
+            instance = new ConsoleInput();
+        }
+        return instance;
+    }
+    public String getLine(String prompt){
+        String input;
+        do{
+            System.out.println(prompt);
+            input = this.scanner.nextLine();
+
+        } while(input == null || input.isEmpty());
+        return input;
+    }
+}
