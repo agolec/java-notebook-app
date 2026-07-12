8fc44ba4c70ef93770f2cd95b89852776aa57d11
Adam Golec <algolec1990@yahoo.com>
2026-07-12 12:49:30 -0400

    stablize input parser

--- src/main/java/com/example/notebookapp/commandparse/CommandParseMain.java
+++ src/main/java/com/example/notebookapp/commandparse/CommandParseMain.java
@@ -1,6 +1,10 @@
 package com.example.notebookapp.commandparse;
 
 import com.example.notebookapp.commandparse.console.input.ConsoleInput;
+import com.example.notebookapp.commandparse.console.input.InputParser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class CommandParseMain {
     public static void main(String[] args){
@@ -8,6 +12,16 @@ public class CommandParseMain {
 
         String input = in.getLine("Enter your string: ");
         System.out.println("You entered '" + input + "'");
+
+        List<String> tokens = Arrays.asList(InputParser.parseUserInput(input));
+
+        System.out.println("Your tokens are: ");
+        System.out.println();
+        for(String token: tokens){
+            System.out.println(token);
+        }
+        System.out.println();
+        System.out.println("end");;
 
     }
 }
